cf829803bb25595d8e410a64291fb63d07c24b26
jaewonjjang <wony3908@naver.com>
2023-01-17 18:37:38 +0900

    사이드 메뉴 시간 기록 보여주기 구현

--- js_file/drag_and_drop.js
+++ js_file/drag_and_drop.js
@@ -1,74 +1,99 @@
-{/* <div id="outerContainer">
-   <div id="container">
-      <div id="item">
- 
-      </div>
-   </div>
-</div>
- 
-<script> */}
+/* Drag and Drop
+1. 카드 위에서 mousedown이벤트가 발생하면 잔상이 남아야된다.
+2. 마우스 위치가 이동하면 잔상은 그자리에 있고, 마우스 위치에 따라 카드가 이동해야 된다
+    -> 마우스 위치가 일정 지점을 넘어가면 넘어간 쪽의 column으로 카드 위치를 이동
+3. mouseup 이벤트가 발생하면 카드를 잔상이 생긴 지점에 넣는다. 
+4. 저장된 데이터에 접근해서(선택된 card의 title, contents 이용하고 parents id 이용하면 될듯...?)
+5. 이동이 끝나면 우측 메뉴 업데이트 작업
+*/
 
+const todo_table = document.getElementsByClassName("_todo_table")[0];
 
-var dragItem = document.querySelector("#_header");
-var container = document.querySelector("#");
-
-var active = false;
-var currentX;
-var currentY;
-var initialX;
-var initialY;
-var xOffset = 0;
-var yOffset = 0;
-
-container.addEventListener("touchstart", dragStart, false);
-container.addEventListener("touchend", dragEnd, false);
-container.addEventListener("touchmove", drag, false);
-
-container.addEventListener("mousedown", dragStart, false);
-container.addEventListener("mouseup", dragEnd, false);
-container.addEventListener("mousemove", drag, false);
-
-function dragStart(e) {
-    if (e.type === "touchstart") {
-        initialX = e.touches[0].clientX - xOffset;
-        initialY = e.touches[0].clientY - yOffset;
-    } else {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
+todo_table.addEventListener("mousedown", (e) => {
+    const cursoroncard = e.target.closest("._col_elem");
+    if (cursoroncard != null) {
+      alert("jaewon");
     }
+});
 
-    if (e.target === dragItem) {
-        active = true;
-    }
-}
 
-function dragEnd(e) {
-    initialX = currentX;
-    initialY = currentY;
+// const moveContainer = (e) => {
+//   if (!e.target.classList.contains("item")) return;
+//   let shiftX = e.clientX - e.target.getBoundingClientRect().left;
+//   let shiftY = e.clientY - e.target.getBoundingClientRect().top;
 
-    active = false;
-}
+//   e.target.style.position = 'absolute';
+//   e.target.style.zIndex = 1000;
+//   section.append(e.target);
 
-function drag(e) {
-    if (active) {
+//   moveAt(e.pageX, e.pageY);
 
-        e.preventDefault();
+//   function moveAt(pageX, pageY) {
+//     e.target.style.left = pageX - shiftX + 'px';
+//     e.target.style.top = pageY - shiftY + 'px';
+//   }
 
-        if (e.type === "touchmove") {
-        currentX = e.touches[0].clientX - initialX;
-        currentY = e.touches[0].clientY - initialY;
-        } else {
-        currentX = e.clientX - initialX;
-        currentY = e.clientY - initialY;
-        }
 
-        xOffset = currentX;
-        yOffset = currentY;
+//   function onMouseMove(event) {
 
-        setTranslate(currentX, currentY, dragItem);
-    }
-}
+//     moveAt(event.pageX, event.pageY);
 
-function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-}
+//     e.target.hidden = true;
+//     let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+//     e.target.hidden = false;
+
+//     if (!elemBelow) return;
+
+//     let droppableBelow = elemBelow.closest('.droppable');
+//     if (currentDroppable != droppableBelow) {
+//       if (currentDroppable) {
+//         leaveDroppable(currentDroppable);
+
+//       }
+//       currentDroppable = droppableBelow;
+//       if (currentDroppable) {
+//         enterDroppable(currentDroppable);
+//         appendSideElement(e.target, droppableBelow);
+
+
+//       }
+//     }
+//   }
+
+//   document.addEventListener('mousemove', onMouseMove);
+
+//   e.target.onmouseup = function () {
+//     document.removeEventListener('mousemove', onMouseMove);
+//     e.target.onmouseup = null;
+//   };
+
+// };
+
+// function enterDroppable(elem) {
+//   elem.style.borderColor = 'blue';
+// }
+
+// function appendSideElement(elem, target) {
+//   const temp = elem;
+//   temp.style = "";
+//   temp.style.transition = 'transition: all 3s linear;';
+//   temp.className = "mini";
+//   elem.remove();
+//   target.append(temp);
+// }
+
+// function appendMainElement(elem) {
+//   elem.className = "mini";
+//   side.append(elem);
+
+// }
+
+// function leaveDroppable(elem) {
+//   elem.style.background = '';
+// }
+
+// con.ondragstart = function () {
+//   return false;
+// };
+
+// con.addEventListener('mousedown', moveContainer);
--- js_file/side_menu.js
+++ js_file/side_menu.js
@@ -43,69 +43,73 @@ function menu_invisible(){
 	side_menu.style.visibility = "hidden";
 }
 
-side_menu_card.prototype.update_side_menu_add = function(id, title) {
+// side_menu_card.prototype.update_side_menu_add = function(id, title) {
     
-    const added_menu = document.getElementById("menu_close_button");
+//     const added_menu = document.getElementById("menu_close_button");
 
-    const new_menu = document.createElement("div");
-    new_menu.classList.add("side_card");
+//     const new_menu = document.createElement("div");
+//     new_menu.classList.add("side_card");
 
-    new_menu.innerHTML = `<div class="side_card">
-                                <div class="side_card_imoji">
-                                    &#128129
-                                </div>
-                                <div class="side_card_contents">
-                                    <div class="contents_author" style="font-size:13px">
-                                        @jaewon
-                                    </div>
-                                    <div class="contents_title" style="font-size:15px">
-                                    ${id}에 ${title.value}를 등록하였습니다
-                                    </div>
-                                    <div class="contents_time" style="font-size:12px">
-                                        1분전
-                                    </div>
-                                </div>
-                            </div>`;
+//     new_menu.innerHTML = `<div class="side_card">
+//                                 <div class="side_card_imoji">
+//                                     &#128129
+//                                 </div>
+//                                 <div class="side_card_contents">
+//                                     <div class="contents_author" style="font-size:13px">
+//                                         @jaewon
+//                                     </div>
+//                                     <div class="contents_title" style="font-size:15px">
+//                                     ${id}에 ${title.value}를 등록하였습니다
+//                                     </div>
+//                                     <div class="contents_time" style="font-size:12px">
+//                                         1분전
+//                                     </div>
+//                                 </div>
+//                             </div>`;
   
-    added_menu.after(new_menu);
-}
+//     added_menu.after(new_menu);
+// }
 
-side_menu_card.prototype.update_side_menu_add = function(id, value) {
-    const deleted_menu = document.getElementById("menu_close_button");
+// side_menu_card.prototype.update_side_menu_add = function(id, value) {
+//     const deleted_menu = document.getElementById("menu_close_button");
 
-    const new_menu = document.createElement("div");
-    new_menu.classList.add("side_card");
+//     const new_menu = document.createElement("div");
+//     new_menu.classList.add("side_card");
 
-    new_menu.innerHTML = `<div class="side_card">
-                                <div class="side_card_imoji">
-                                    &#128129
-                                </div>
-                                <div class="side_card_contents">
-                                    <div class="contents_author" style="font-size:13px">
-                                        @jaewon
-                                    </div>
-                                    <div class="contents_title" style="font-size:15px">
-                                        ${id}에 ${value}를 삭제하였습니다
-                                    </div>
-                                    <div class="contents_time" style="font-size:12px">
-                                        1분전
-                                    </div>
-                                </div>
-                            </div>`;
+//     new_menu.innerHTML = `<div class="side_card">
+//                                 <div class="side_card_imoji">
+//                                     &#128129
+//                                 </div>
+//                                 <div class="side_card_contents">
+//                                     <div class="contents_author" style="font-size:13px">
+//                                         @jaewon
+//                                     </div>
+//                                     <div class="contents_title" style="font-size:15px">
+//                                         ${id}에 ${value}를 삭제하였습니다
+//                                     </div>
+//                                     <div class="contents_time" style="font-size:12px">
+//                                         1분전
+//                                     </div>
+//                                 </div>
+//                             </div>`;
 
-    deleted_menu.after(new_menu);
+//     deleted_menu.after(new_menu);
    
-}
+// }
 
 function update_side_menu(close_menu){
-    const date = new Date();
 
-    console.log(side_menu_arr.card_array.length);
-    
+    flush_prev_menu();
+
+    let date = new Date();
+
     for(let i=0;i<side_menu_arr.card_array.length;i++){
-        const newp = document.createElement("div");
-        newp.className = "side_card";
-        newp.innerHTML = `<div class="side_card_imoji">&#128129</div>
+        if(side_menu_arr.card_array[i].action == "추가"){
+            let time_collaps = time_collap_for_new_card(date, side_menu_arr.card_array[i].time)
+            let newp = document.createElement("div");
+            newp.className = "side_card";
+            newp.innerHTML = '';
+            newp.innerHTML = `<div class="side_card_imoji">&#128129</div>
                                 <div class="side_card_contents">
                                     <div class="contents_author" style="font-size:13px">
                                         @jaewon
@@ -114,11 +118,49 @@ function update_side_menu(close_menu){
                                         ${side_menu_arr.card_array[i].col_id1}에 ${side_menu_arr.card_array[i].new_title}을 등록하였습니다
                                     </div>
                                     <div class="contents_time" style="font-size:12px">
-                                        시간 계산
+                                        ${time_collaps}분전
                                     </div>
                                 </div>`;
-        close_menu.after(newp);
+            close_menu.after(newp);
+        }
+        else if(side_menu_arr.card_array[i].action == "삭제"){
+            let time_collaps = time_collap_for_new_card(date, side_menu_arr.card_array[i].time)
+            let newp = document.createElement("div");
+            newp.className = "side_card";
+            newp.innerHTML = '';
+            newp.innerHTML = `<div class="side_card_imoji">&#128129</div>
+                                <div class="side_card_contents">
+                                    <div class="contents_author" style="font-size:13px">
+                                        @jaewon
+                                    </div>
+                                    <div class="contents_title" style="font-size:15px">
+                                        ${side_menu_arr.card_array[i].col_id1}에 ${side_menu_arr.card_array[i].new_title}을 삭제하였습니다
+                                    </div>
+                                    <div class="contents_time" style="font-size:12px">
+                                        ${time_collaps}분전
+                                    </div>
+                                </div>`;
+            close_menu.after(newp);
+        }
+        else if(side_menu_arr.card_array[i].action == "이동"){
+
+        }
+        else return;ㄴㅇ
     }
 }
 
-export {/*update_side_menu_add, update_side_menu_delete,*/ side_menu_card, side_menu_arr};
+function flush_prev_menu(){
+    const prev_cards = document.getElementsByClassName("side_card");
+    while(prev_cards.length){
+        prev_cards[0].remove();
+    }
+}
+function time_collap_for_new_card(cur_time, card_time){
+    const time_diff = (cur_time.getTime() - card_time.getTime()) / 60000;
+
+    if(time_diff < 1) return 0;
+    else return Math.floor(time_diff); 
+}
+export {side_menu_card, side_menu_arr};
+
+
